e2b90916a38c35606788065d07d8ffb6e4e9306b
joey <>
2017-05-26 18:32:06 +0800

    增加多状态切换视图的功能

--- app/src/main/java/com/joey/fastdemo/fragment/CategoryFragment.java
+++ app/src/main/java/com/joey/fastdemo/fragment/CategoryFragment.java
@@ -8,7 +8,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.classic.common.MultipleStatusView;
 import com.joey.fastdemo.R;
 
 
@@ -19,8 +21,8 @@ import com.joey.fastdemo.R;
 public class CategoryFragment extends Fragment
 {
 
-    Button button;
-    TextView textView;
+    private MultipleStatusView mMultipleStatusView;
+    Button btnContent,btnLoading,btnEmpty,btnError,btnEx;
 
     @Nullable
     @Override
@@ -32,15 +34,50 @@ public class CategoryFragment extends Fragment
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        button = (Button) view.findViewById(R.id.btnCrash);
-        button.setOnClickListener(new View.OnClickListener() {
+        mMultipleStatusView = (MultipleStatusView)view.findViewById(R.id.main_multiplestatusview);
+        mMultipleStatusView.setOnRetryClickListener(onRetryClickListener);
+        mMultipleStatusView.showLoading();
+        btnContent = (Button) view.findViewById(R.id.btnContent);
+        btnLoading = (Button) view.findViewById(R.id.btnLoading);
+        btnEmpty = (Button) view.findViewById(R.id.btnEmpty);
+        btnError = (Button) view.findViewById(R.id.btnError);
+        btnEx = (Button) view.findViewById(R.id.btnEx);
+        btnContent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText("djkhk");
+                mMultipleStatusView.showContent();
+            }
+        });
+        btnLoading.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMultipleStatusView.showLoading();
+            }
+        });
+        btnEmpty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMultipleStatusView.showEmpty();
+            }
+        });
+        btnError.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMultipleStatusView.showError();
+            }
+        });
+        btnEx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMultipleStatusView.showNoNetwork();
             }
         });
     }
-
+    private final View.OnClickListener onRetryClickListener = new View.OnClickListener() {
+        @Override public void onClick(View v) {
+            Toast.makeText(getContext(), "您点击了重试视图", Toast.LENGTH_SHORT).show();
+            mMultipleStatusView.showLoading();
+        }
+    };
 
 }
